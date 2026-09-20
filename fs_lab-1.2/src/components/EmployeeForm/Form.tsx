ed9443651c655106ee031c6c617d0782864cf22e
sboyer2 <sboyer2@academic.rrc.ca>
2026-09-19 23:47:01 -0500

    Victory: I figured out how to type the submit event so ts would stop SCREAMING at me

--- fs_lab-1.2/src/components/EmployeeForm/Form.tsx
+++ fs_lab-1.2/src/components/EmployeeForm/Form.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
+
+interface employeeData {
+    firstName: string
+    lastName: string
+    department: 'Administration' | 'Audit' | 'Banking Operations' |
+    'Communications' | 'Corporate Services' | 'Facilities' | 'Financial Services' |
+    'Human Resources' | 'Information Technology' | 'It Technician'
+
+}
 function EmployeeForm () {
+    function handleSubmission(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+    }
     return(
-        <form action="submit">
+        <form onSubmit={handleSubmission}>
             <label>Employee First Name:
-                <input type="text" name="EmployeeFirstName" id="FirstName" />
+                <input type="text" name="employeeFirstName" />
             </label>
             <label>Employee Last Name:
-                <input type="text" name="EmployeeLastName" id="LastName" />
+                <input type="text" name="employeeLastName" />
             </label>
             <label> Submit New Employee:
-                <input type="button" value="submit" />
+                <button type="submit"></button>
             </label>
 
         </form>
